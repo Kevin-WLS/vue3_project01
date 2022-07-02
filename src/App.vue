1f43be8e5f3fb61391cb27db344864df2d15fe6d
wanglish <739100735@qq.com>
2022-07-02 14:46:08 +0800

    feat: 项目搭建

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  <div>1235995</div>
+  <div>
+    <router-link to="/login">login</router-link>
+    <router-link to="/main">main</router-link>
+
+    <router-view></router-view>
+
+    <h2>{{ $store.state.name }}</h2>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
 
-  components: {
-    HelloWorld
-  }
+  components: {}
 })
 </script>
 
---
+++ src/router/index.ts
@@ -0,0 +1,26 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+// 导入类型
+import type { RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: () => import('@/views/login/index.vue')
+  },
+  {
+    path: '/main',
+    component: () => import('@/views/main/index.vue')
+  }
+]
+
+const router = createRouter({
+  routes,
+  history: createWebHashHistory()
+})
+
+export default router
